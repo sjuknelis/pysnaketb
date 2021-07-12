f9b79bfcac04e8b00495cb200f27ce82eb0e18de
sjuknelis <sjuknelis24@nobles.edu>
2021-07-12 19:37:26 -0400

    Animated drawer and better movement

--- game.py
+++ game.py
@@ -2,10 +2,19 @@ class Snake:
     head = (5,3)
     path = [(-1,0),(-1,0),(0,1),(0,1),(1,0),(1,0),(0,1),(0,1),(-1,0),(-1,0)]
     direction = (1,0)
-    next_direction = (1,0)
+    move_buffer = []
 
     def update(self):
-        direction = next_direction
         self.head = (self.head[0] + self.direction[0],self.head[1] + self.direction[1])
         self.path.insert(0,(-self.direction[0],-self.direction[1]))
         self.path.pop()
+        
+        if len(self.move_buffer) > 0:
+            for (index,item) in enumerate(self.move_buffer):
+                if item != self.direction and item != (-self.direction[0],-self.direction[1]):
+                    self.direction = item
+                    del self.move_buffer[index]
+                    break
+
+    def queue(self,direction):
+        self.move_buffer.insert(0,direction)
--- main.py
+++ main.py
@@ -9,23 +9,50 @@ def render(screen,fnum):
     im = Image.new("RGBA",(1200,60),(0,0,0,255))
     draw = ImageDraw.Draw(im)
 
+    scroll = snake.head[1] + snake.direction[1] * 0.2 * fnum
+
     pos = list(snake.head)
     last = list(pos)
-    for item in snake.path:
+    for (index,item) in enumerate(snake.path):
         pos[0] += item[0]
         pos[1] += item[1]
+        if index == len(snake.path) - 1:
+            pos[0] -= item[0] * 0.2 * fnum
+            pos[1] -= item[1] * 0.2 * fnum
         if abs(pos[1] - snake.head[1]) <= 1:
             draw.rectangle(
                 [
                     min(pos[0],last[0]) * 60 + 10,
-                    (min(pos[1],last[1]) - snake.head[1]) * 60 + 10,
+                    (min(pos[1],last[1]) - scroll) * 60 + 10,
                     max(pos[0],last[0]) * 60 + 50,
-                    (max(pos[1],last[1]) - snake.head[1]) * 60 + 50
+                    (max(pos[1],last[1]) - scroll) * 60 + 50
                 ],
                 fill=(0,255,0,255)
             )
         last = list(pos)
-        
+
+    ext = (snake.head[0] + snake.direction[0] * 0.2 * fnum,snake.head[1] + snake.direction[1] * 0.2 * fnum)
+    draw.rectangle(
+        [
+            min(snake.head[0],ext[0]) * 60 + 10,
+            (min(snake.head[1],ext[1]) - scroll) * 60 + 10,
+            max(snake.head[0],ext[0]) * 60 + 50,
+            (max(snake.head[1],ext[1]) - scroll) * 60 + 50
+        ],
+        fill=(0,255,0,255)
+    )
+
+    if snake.head[1] <= 2:
+        draw.rectangle(
+            [10,(- scroll) * 60,1190,(- scroll + 1) * 60 + 5],
+            fill=(0,255,0,255)
+        )
+    if snake.head[1] >= 18:
+        draw.rectangle(
+            [10,(20 - scroll) * 60 - 5,1190,(20 - scroll + 1) * 60],
+            fill=(0,255,0,255)
+        )
+
     draw.rectangle(
         [0,0,60,60],
         fill=(0,255,0,255)
@@ -42,7 +69,7 @@ def render(screen,fnum):
 def f(button):
     print("f")
 
-def pmain():
+def main():
     pygame.init()
     screen = pygame.display.set_mode((200,200))
 
@@ -63,22 +90,24 @@ def pmain():
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_UP:
-                    snake.next_direction = (0,-1)
+                    snake.queue((0,-1))
                 elif event.key == pygame.K_DOWN:
-                    snake.next_direction = (0,1)
+                    snake.queue((0,1))
                 elif event.key == pygame.K_LEFT:
-                    snake.next_direction = (-1,0)
+                    snake.queue((-1,0))
                 elif event.key == pygame.K_RIGHT:
-                    snake.next_direction = (1,0)
-        render(screen,fnum)
-        pygame.display.update()
+                    snake.queue((1,0))
 
         fnum += 1
         fnum %= 5
-        if fnum == 4:
+        if fnum == 0:
             snake.update()
+
+        render(screen,fnum)
+        pygame.display.update()
         button.image = "game.png"
+
         time.sleep(1/30)
 
 if __name__ == "__main__":
-    pmain()
+    main()
