72280818453854f23c391be2cd93875bd9722059
sjuknelis <sjuknelis24@nobles.edu>
2021-07-12 19:42:50 -0400

    Refactor into classes

--- game.py
+++ game.py
@@ -8,7 +8,7 @@ class Snake:
         self.head = (self.head[0] + self.direction[0],self.head[1] + self.direction[1])
         self.path.insert(0,(-self.direction[0],-self.direction[1]))
         self.path.pop()
-        
+
         if len(self.move_buffer) > 0:
             for (index,item) in enumerate(self.move_buffer):
                 if item != self.direction and item != (-self.direction[0],-self.direction[1]):
@@ -18,3 +18,64 @@ class Snake:
 
     def queue(self,direction):
         self.move_buffer.insert(0,direction)
+
+    def render(self,draw,fnum):
+        scroll = self.head[1] + self.direction[1] * 0.2 * fnum
+        pos = list(self.head)
+        last = list(pos)
+        for (index,item) in enumerate(self.path):
+            pos[0] += item[0]
+            pos[1] += item[1]
+            if index == len(self.path) - 1:
+                pos[0] -= item[0] * 0.2 * fnum
+                pos[1] -= item[1] * 0.2 * fnum
+            if abs(pos[1] - self.head[1]) <= 1:
+                draw.rectangle(
+                    [
+                        min(pos[0],last[0]) * 60 + 10,
+                        (min(pos[1],last[1]) - scroll) * 60 + 10,
+                        max(pos[0],last[0]) * 60 + 50,
+                        (max(pos[1],last[1]) - scroll) * 60 + 50
+                    ],
+                    fill=(0,255,0,255)
+                )
+            last = list(pos)
+
+        ext = (self.head[0] + self.direction[0] * 0.2 * fnum,self.head[1] + self.direction[1] * 0.2 * fnum)
+        draw.rectangle(
+            [
+                min(self.head[0],ext[0]) * 60 + 10,
+                (min(self.head[1],ext[1]) - scroll) * 60 + 10,
+                max(self.head[0],ext[0]) * 60 + 50,
+                (max(self.head[1],ext[1]) - scroll) * 60 + 50
+            ],
+            fill=(0,255,0,255)
+        )
+
+class Game:
+    snake = Snake()
+    apple = (10,10)
+
+    def render(self,draw,fnum):
+        scroll = self.snake.head[1] + self.snake.direction[1] * 0.2 * fnum
+
+        self.snake.render(draw,fnum)
+
+        if self.snake.head[1] <= 2:
+            draw.rectangle(
+                [10,(- scroll) * 60,1190,(- scroll + 1) * 60 + 5],
+                fill=(0,255,0,255)
+            )
+        if self.snake.head[1] >= 18:
+            draw.rectangle(
+                [10,(20 - scroll) * 60 - 5,1190,(20 - scroll + 1) * 60],
+                fill=(0,255,0,255)
+            )
+        draw.rectangle(
+            [0,0,60,60],
+            fill=(0,255,0,255)
+        )
+        draw.rectangle(
+            [1140,0,1200,60],
+            fill=(0,255,0,255)
+        )
--- main.py
+++ main.py
@@ -1,67 +1,14 @@
 import sys,pygame,time
 import PyTouchBar as tb
 from PIL import Image,ImageDraw
-from game import Snake
+from game import Game
 
-snake = Snake()
+game = Game()
 
 def render(screen,fnum):
     im = Image.new("RGBA",(1200,60),(0,0,0,255))
     draw = ImageDraw.Draw(im)
-
-    scroll = snake.head[1] + snake.direction[1] * 0.2 * fnum
-
-    pos = list(snake.head)
-    last = list(pos)
-    for (index,item) in enumerate(snake.path):
-        pos[0] += item[0]
-        pos[1] += item[1]
-        if index == len(snake.path) - 1:
-            pos[0] -= item[0] * 0.2 * fnum
-            pos[1] -= item[1] * 0.2 * fnum
-        if abs(pos[1] - snake.head[1]) <= 1:
-            draw.rectangle(
-                [
-                    min(pos[0],last[0]) * 60 + 10,
-                    (min(pos[1],last[1]) - scroll) * 60 + 10,
-                    max(pos[0],last[0]) * 60 + 50,
-                    (max(pos[1],last[1]) - scroll) * 60 + 50
-                ],
-                fill=(0,255,0,255)
-            )
-        last = list(pos)
-
-    ext = (snake.head[0] + snake.direction[0] * 0.2 * fnum,snake.head[1] + snake.direction[1] * 0.2 * fnum)
-    draw.rectangle(
-        [
-            min(snake.head[0],ext[0]) * 60 + 10,
-            (min(snake.head[1],ext[1]) - scroll) * 60 + 10,
-            max(snake.head[0],ext[0]) * 60 + 50,
-            (max(snake.head[1],ext[1]) - scroll) * 60 + 50
-        ],
-        fill=(0,255,0,255)
-    )
-
-    if snake.head[1] <= 2:
-        draw.rectangle(
-            [10,(- scroll) * 60,1190,(- scroll + 1) * 60 + 5],
-            fill=(0,255,0,255)
-        )
-    if snake.head[1] >= 18:
-        draw.rectangle(
-            [10,(20 - scroll) * 60 - 5,1190,(20 - scroll + 1) * 60],
-            fill=(0,255,0,255)
-        )
-
-    draw.rectangle(
-        [0,0,60,60],
-        fill=(0,255,0,255)
-    )
-    draw.rectangle(
-        [1140,0,1200,60],
-        fill=(0,255,0,255)
-    )
-
+    game.render(draw,fnum)
     im.save("game.png")
 
     screen.fill((255,255,255))
@@ -90,18 +37,18 @@ def main():
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_UP:
-                    snake.queue((0,-1))
+                    game.snake.queue((0,-1))
                 elif event.key == pygame.K_DOWN:
-                    snake.queue((0,1))
+                    game.snake.queue((0,1))
                 elif event.key == pygame.K_LEFT:
-                    snake.queue((-1,0))
+                    game.snake.queue((-1,0))
                 elif event.key == pygame.K_RIGHT:
-                    snake.queue((1,0))
+                    game.snake.queue((1,0))
 
         fnum += 1
         fnum %= 5
         if fnum == 0:
-            snake.update()
+            game.snake.update()
 
         render(screen,fnum)
         pygame.display.update()
